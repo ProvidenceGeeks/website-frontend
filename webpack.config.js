962163aa9650fca42979e41a832245e77e83df0f
Peter Holyoke <pholyoke@kenzan.com>
2017-10-03 13:39:27 -0400

    Add eslint and .eslintrc file and eslint-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,34 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 var path = require('path');
 var webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    index: './src/index.js'
+  },
+
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: 'app.bundle.js'
+    filename: 'app.bundle.js',
+    sourceMapFilename: '[name].map'
   },
 
   module: {
-    loaders: [
+    rules: [
+      {
+        test: /\.js$/,
+        enforce: 'pre',
+        loader: 'eslint-loader',
+        exclude: path.join(__dirname, 'node_modules')
+      },
       {
         test: /\.js$/,
         loader: 'babel-loader',
         query: {
-          presets: ['react',  'babel-preset-env']
+          presets: [
+            'babel-preset-env',
+            'react'
+          ]
         },
         exclude: path.join(__dirname, 'node_modules')
       }
