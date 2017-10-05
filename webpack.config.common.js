a8699385f376acba2f1f04e3bf5787f34cec4090
Peter Holyoke <pholyoke@kenzan.com>
2017-10-05 11:55:32 -0400

    Set up commons chunk plugin, HMR (Hot module replacement) and bootstrap-react dependency

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -1,19 +1,29 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   entry: {
-    index: './src/index.js'
+    index: './index.js',
+    vendor: './vendor.js'
   },
 
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, './build'),
     filename: '[name].[chunkhash].bundle.js',
-    sourceMapFilename: '[name].map'
+    sourceMapFilename: '[name].map',
+    chunkFilename: '[id].chunk.js'
   },
+
+  context: path.resolve(__dirname, 'src'),
 
   module: {
     rules: [
+      {
+        test: /\.js$/,
+        loader: 'react-hot-loader/webpack',
+        exclude: path.join(__dirname, 'node_modules')
+      },
       {
         test: /\.js$/,
         enforce: 'pre',
@@ -37,8 +47,19 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin(
       {
-        template: './src/index.html'
+        template: './index.html',
+        chunksSortMode: 'dependency'
       }
-    )
+    ),
+
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['vendor', 'common'],
+      filenames: ['common.js', 'vendor.js'],
+      minChunks: Infinity
+    }),
+
+    new webpack.ProvidePlugin({ // exposes non-modular vendor globals to webpack
+      jQuery: 'jquery'
+    })
   ]
 };
