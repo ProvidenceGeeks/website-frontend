37828bb78d1bea344e9faa8ef8c7c15420f08f9e
Peter Holyoke <pholyoke@kenzan.com>
2017-10-18 14:06:00 -0400

    Add changes requested in PR

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,25 +4,27 @@ import './header.scss';
 import Logo from '../../components/logo/logo';
 import SlackLogo from '../../images/Slack-Monochrome-White.svg';
 
-export default class Hello extends React.Component {
+export default class Header extends React.Component {
   constructor() {
     super();
   }
 
   render() {
     return (
-      <div className="row-fluid">
-        <div id="header" className="col-xs-12">
-          <div className="col-xs-2 logo">
-            <Logo />
+      <div className="row">
+        <div className="header">
+          <div className="col-xs-2">
+            <div className="logo">
+              <Logo />
+            </div>
           </div>
 
-          <div className="col-xs-7">&nbsp;</div>
-
-          <div className="social col-xs-3">
-            <div><a href="https://www.facebook.com/providencegeeks/" title="Like Us"><Icon className="circle" name="facebook" /></a></div>
-            <div><a href="https://twitter.com/providencegeeks" title="Follow Us"><Icon className="circle" name="twitter" /></a></div>
-            <div><a href="http://providencegeeks.slack.com/" title="Join Us"><img className="slack" src={ SlackLogo } /></a></div>
+          <div className="social-container col-xs-8 col-xs-offset-2">
+            <ul className="social list-inline">
+              <li><a href="https://www.facebook.com/providencegeeks/" title="Like Us"><Icon className="circle" name="facebook" /></a></li>
+              <li><a href="https://twitter.com/providencegeeks" title="Follow Us"><Icon className="circle" name="twitter" /></a></li>
+              <li><a href="http://providencegeeks.slack.com/" title="Join Us"><img className="slack" src={ SlackLogo } /></a></li>
+            </ul>
           </div>
         </div>
       </div>
--- src/components/header/header.spec.js
+++ src/components/header/header.spec.js
@@ -3,14 +3,14 @@ import raf from '../../../test/shims/react-raf-shim';
 import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-import Hello from './header';
+import Header from './header';
 
 configure({ adapter: new Adapter() });
 
 describe('Header Component', () => {
-  const hello = shallow(<Hello />);
+  const header = shallow(<Header />);
 
   it('should not be null', () => {
-    expect(hello).not.toBeNull();
+    expect(header).not.toBeNull();
   });
 });
