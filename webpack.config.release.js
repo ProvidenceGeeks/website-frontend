132ed8f56ccad9c3d1101c85c4f4c9c94967ae63
Owen Buckley <obuckley@kenzan.com>
2017-10-20 15:41:11 -0400

    Technical/issue 3 staging environment (#19)
    * WIP
    
    * setup release building
    
    * fix syntax

---
+++ webpack.config.release.js
@@ -0,0 +1,35 @@
+const webpackMerge = require('webpack-merge');
+const prodConfig = require('./webpack.config.prod');
+const S3Plugin = require('webpack-s3-plugin');
+const bucketBase = 'providencegeeks.com/frontend';
+const isProductionRelease = process.env.RELEASE_ENV === 'production';
+const releaseConfig = {
+  bucket: isProductionRelease ? 'production' : 'stage',
+  cdnBase: isProductionRelease ? 'http://dt3s8ap74m6pw.cloudfront.net' : 'http://d2b64jbw6hxz4f.cloudfront.net',
+  distributionId: isProductionRelease ? process.env.AWS_DISTRIBUTION_ID_PROD : process.env.AWS_DISTRIBUTION_ID_STAGE
+};
+
+module.exports = webpackMerge(prodConfig, {
+
+  plugins: [
+
+    new S3Plugin({
+      s3Options: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        region: 'us-east-1'
+      },
+      s3UploadOptions: {
+        Bucket: `${bucketBase}/${releaseConfig.bucket}`
+      },
+      cdnizerOptions: {
+        defaultCDNBase: releaseConfig.cdnBase
+      },
+      cloudfrontInvalidateOptions: {
+        DistributionId: releaseConfig.distributionId,
+        Items: ['/index.html']
+      }
+    })
+
+  ]
+});
