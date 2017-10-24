ba0f60bebaa6793477e61b68e364ca6227bd2994
Owen Buckley <obuckley@kenzan.com>
2017-10-24 10:04:52 -0400

    fix unit test

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -24,7 +24,7 @@ export default class Header extends React.Component {
             <ul className="social list-inline">
               <li><a className="facebook-link" href="https://www.facebook.com/providencegeeks/" title="Like Us" target="_blank" rel="noopener noreferrer"><FacebookIcon /></a></li>
               <li><a className="slack-link" href="http://providencegeeks.slack.com/" title="Join Us" target="_blank" rel="noopener noreferrer"><SlackIcon /></a></li>
-              <li><a className="twitter-link" href="https://twitter.com/providencegeeks" title="Follow Us" rel="noopener noreferrer"><TwitterIcon /></a></li>
+              <li><a className="twitter-link" href="https://twitter.com/providencegeeks" title="Follow Us" target="_blank" rel="noopener noreferrer"><TwitterIcon /></a></li>
             </ul>
           </div>
         </div>
