0f288150e14aa2c551c366961cc534481d35ccf8
Peter Holyoke <pholyoke@kenzan.com>
2017-10-24 09:43:51 -0400

    Upgrade to bootstrap 4

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -13,15 +13,15 @@ export default class Header extends React.Component {
   render() {
     return (
       <div className="row">
-        <div className="header">
-          <div className="col-xs-2">
+        <div className="header d-flex">
+          <div className="col-md-2">
             <div className="logo">
               <a href="/"><PvdGeeksLogo /></a>
             </div>
           </div>
 
-          <div className="social-container col-xs-8 col-xs-offset-2">
-            <ul className="social list-inline">
+          <div className="social-container col-md-8 offset-md-2 d-flex justify-content-end align-items-center">
+            <ul className="social list-inline d-flex">
               <li><a className="facebook-link" href="https://www.facebook.com/providencegeeks/" title="Like Us" target="_blank" rel="noopener noreferrer"><FacebookIcon /></a></li>
               <li><a className="slack-link" href="http://providencegeeks.slack.com/" title="Join Us" target="_blank" rel="noopener noreferrer"><SlackIcon /></a></li>
               <li><a className="twitter-link" href="https://twitter.com/providencegeeks" title="Follow Us" target="_blank" rel="noopener noreferrer"><TwitterIcon /></a></li>
