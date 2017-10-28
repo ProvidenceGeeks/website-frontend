195d1a829663c4916df4fd92598ef3793090f307
Owen Buckley <owen.buckley@analogstudios.net>
2017-10-28 13:41:02 -0400

    added favicons plugin and logo

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,7 +1,8 @@
 const commonConfig = require('./webpack.config.common');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 const webpackMerge = require('webpack-merge');
 const WebpackMd5Hash = require('webpack-md5-hash');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = webpackMerge(commonConfig, {
 
@@ -19,6 +20,28 @@ module.exports = webpackMerge(commonConfig, {
 
   plugins: [
     new WebpackMd5Hash(),
+
+    new FaviconsWebpackPlugin({
+      logo: './components/bootstrap/images/pvd-geeks-logo.png',
+      emitStats: true,
+      prefix: 'icons/',
+      statsFilename: 'icons/stats.json',
+      inject: true,
+      title: 'The Greenhouse',
+      background: '#efefef',
+      icons: {
+        android: true,
+        appleIcon: true,
+        appleStartup: true,
+        coast: false,
+        favicons: true,
+        firefox: true,
+        opengraph: true,
+        twitter: true,
+        yandex: true,
+        windows: true
+      }
+    }),
 
     new ExtractTextPlugin('styles.[chunkhash].css')
   ]
