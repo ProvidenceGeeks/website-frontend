ce439e7a55f3ba0ba54198cabb24eb77ab1ee15d
Owen Buckley <obuckley@kenzan.com>
2017-11-04 17:46:22 -0400

    created an events service

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,4 @@
+import './vendor';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Bootstrap from './components/bootstrap/bootstrap';
--- src/views/home/home.jsx
+++ src/views/home/home.jsx
@@ -1,11 +1,19 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
 import HeroBanner from '../../components/hero-banner/hero-banner';
+import EventsService from '../../services/events/events-service';
 import './home.scss';
 
 export default class Home extends React.Component {
   constructor() {
     super();
+
+    EventsService.getEvents()
+      .then((events) => {
+        console.debug('events!!!!!!', events); // eslint-disable-line
+      }).catch(function(response) {
+        console.log(response); // eslint-disable-line
+      });
   }
 
   render() {
