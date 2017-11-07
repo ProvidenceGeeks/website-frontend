cff6e061f62bfa6a8b19d552a3cbd81a60b2ac6f
Peter Holyoke <pholyoke@kenzan.com>
2017-11-07 12:09:32 -0500

    Add event card WIP

--- src/components/events-list/events-list.jsx
+++ src/components/events-list/events-list.jsx
@@ -11,7 +11,47 @@ export default class EventsList extends React.Component {
 
     return (
       <div className="row-fluid">
-        <h3 className="events-header">Upcoming Events</h3>
+        <div className="col-md-12">
+          <h3 className="events-header">Upcoming Events</h3>
+        </div>
+
+        <div className="events-grid col-md-12 d-flex justify-content-between">
+
+          <div className="col-md-4">
+            <div className="card">
+              <img className="card-img-top" src="http://via.placeholder.com/318x180" alt="Card image cap" />
+
+              <div className="card-img-overlay d-flex">
+                <div className="event-title-container align-self-end">
+                  <span className="event-title">Event Name</span>
+                </div>
+              </div>
+
+              <div className="card-block">
+                <p className="card-text">
+                  Lorem ipsum dolor sit amet, ex putant scriptorem qui, vim fastidii senserit at.
+                  Mea no postea fastidii. Dicam nemore oporteat vel an.
+                </p>
+
+                <div className="event-footer">
+                  <span className="event-datetime float-left">
+                    <span className="event-date">Date</span>
+                    <span className="event-time">Time</span>
+                  </span>
+
+                  <svg version="1.1" viewBox="0 0 24 24" className="event-social-share float-right">
+                    <g id="info" />
+                    <g id="icons">
+                      <path d="M21.7,10.2l-6.6-6C14.6,3.7,14,4.2,14,5v3c-4.7,0-8.7,2.9-10.6,6.8c-0.7,1.3-1.1,2.7-1.4,4.1
+                      c-0.2,1,1.3,1.5,1.9,0.6C6.1,16,9.8,13.7,14,13.7V17c0,0.8,0.6,1.3,1.1,0.8l6.6-6C22.1,11.4,22.1,10.6,21.7,10.2z" id="share"/>
+                    </g>
+                  </svg>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
       </div>
     );
   }
