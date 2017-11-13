f05f5001650aa13f240576c076bdcb1551b645c5
Owen Buckley <obuckley@kenzan.com>
2017-11-13 13:04:40 -0500

    rebase fix

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -14,12 +14,8 @@ const Footer = () => {
           </div>
         </div>
 
-        <div className="social-container col-md-8 offset-md-2 d-flex justify-content-end align-items-center">
-          <ul className="social list-inline d-flex">
-            <li><a className="facebook-link" href="https://www.facebook.com/providencegeeks/" title="Like Us" target="_blank" rel="noopener noreferrer"><FacebookIcon /></a></li>
-            <li><a className="slack-link" href="http://providencegeeks.slack.com/" title="Join Us" target="_blank" rel="noopener noreferrer"><SlackIcon /></a></li>
-            <li><a className="twitter-link" href="https://twitter.com/providencegeeks" title="Follow Us" target="_blank" rel="noopener noreferrer"><TwitterIcon /></a></li>
-          </ul>
+        <div className="col-md-8 offset-md-2 d-flex justify-content-end align-items-center">
+          <SocialFollowLinks/>
         </div>
       </div>
 
@@ -30,6 +26,7 @@ const Footer = () => {
       <div className="copyright col-md-12">
         <small className="copyright-text">&copy; Copyright 2017 Providence Geeks</small>
       </div>
+
     </div>
 
   );
