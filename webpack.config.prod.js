4b25de447115d7428ea5720c31a96bcd06a79c67
Owen Buckley <owen.buckley@analogstudios.net>
2017-11-18 02:13:40 -0500

    web app manifest

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -4,9 +4,9 @@ const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 const HtmlCriticalPlugin = require('html-critical-webpack-plugin');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const path = require('path');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 const webpack = require('webpack');
 const webpackMerge = require('webpack-merge');
-const WebpackMd5Hash = require('webpack-md5-hash');
 
 module.exports = webpackMerge(commonConfig, {
 
@@ -22,7 +22,6 @@ module.exports = webpackMerge(commonConfig, {
   },
 
   plugins: [
-    new WebpackMd5Hash(),
 
     new FaviconsWebpackPlugin({
       logo: './components/bootstrap/images/pvd-geeks-logo.png',
@@ -31,7 +30,7 @@ module.exports = webpackMerge(commonConfig, {
       statsFilename: 'icons/stats.json',
       inject: true,
       title: 'Providence Geeks',
-      background: '#efefef',
+      background: '#bcbfc2',
       icons: {
         android: true,
         appleIcon: true,
@@ -44,6 +43,21 @@ module.exports = webpackMerge(commonConfig, {
         yandex: true,
         windows: true
       }
+    }),
+
+    new WebpackPwaManifest({
+      name: 'Providence Geeks',
+      short_name: 'PVD Geeks', // eslint-disable-line camelcase
+      start_url: '/', // eslint-disable-line camelcase
+      inject: true,
+      fingerprints: true,
+      ios: true,
+      background_color: '#bcbfc2', // eslint-disable-line camelcase
+      theme_color: '#1a2930', // eslint-disable-line camelcase
+      icons: [{
+        src: path.resolve('./src/components/bootstrap/images/pvd-geeks-logo.png'),
+        sizes: [96, 128, 192, 256, 384, 512]
+      }]
     }),
 
     new ExtractTextPlugin('styles.[chunkhash].css'),
