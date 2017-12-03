90f04ae2596582032ac475172a5a021922db9ca7
Owen Buckley <obuckley@kenzan.com>
2017-12-03 12:23:17 -0500

    import ordering

--- src/views/home/home.jsx
+++ src/views/home/home.jsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
+import BlogPostsLists from '../../components/blog-posts-list/blog-posts-list';
+import EventsList from '../../components/events-list/events-list';
 import HeroBanner from '../../components/hero-banner/hero-banner';
 import NavigationBar from '../../components/navigation-bar/navigation-bar';
 import './home.scss';
-import EventsList from '../../components/events-list/events-list';
-import BlogPostsLists from '../../components/blog-posts-list/blog-posts-list';
 
 const Home = () => {
   return (
