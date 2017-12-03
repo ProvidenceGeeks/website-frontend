871216b99494e6b4ee4dd14dc1e61c45f9e2f0d3
Owen Buckley <obuckley@kenzan.com>
2017-12-03 11:40:01 -0500

    wired up posts service and initial rendering of latest posts

--- src/components/blog-posts-list/blog-posts-list.jsx
+++ src/components/blog-posts-list/blog-posts-list.jsx
@@ -1,11 +1,86 @@
 import * as React from 'react';
+import moment from 'moment';
+import PropTypes from 'prop-types';
+import Card from '../../components/card/card';
+import PostsService from '../../services/posts/posts-service';
+import LoadMoreButton from '../load-more-button/load-more-button';
+import './blog-posts-list.scss';
 
-const BlogPostsList = () => {
-  return (
+export default class BlogPostsList extends React.Component {
+  constructor() {
+    super();
 
-    <h3 className="blog-posts">BLOG POSTS GO HERE</h3>
+    this.state = {
+      posts: [],
+      visiblePosts: [],
+      currentPage: 2
+    };
+  }
 
-  );
+  componentDidMount() {
+    PostsService.getPosts()
+      .then((posts) => {
+        this.setState({
+          posts: posts,
+          visiblePosts: posts.slice(0, 6)
+        });
+      }).catch(function(response) {
+        console.error(response); // eslint-disable-line no-console
+      });
+  }
+
+  loadMorePosts() {
+    this.setState({
+      currentPage: this.state.currentPage + 1,
+      visiblePosts: this.state.posts.slice(0, this.state.currentPage * 6)
+    });
+  }
+
+  static formatHeading(post) {
+    const author = post.author_name ? post.author_name : '';
+    const date = moment(post.date).format('MM/DD/YY h:mmA');
+
+    return `${author} ${date}`;
+  }
+
+  render() {
+    return (
+
+      <div className="row-fluid">
+        <div className="col-md-12">
+          <h3 className="posts-header">Latest Posts</h3>
+        </div>
+
+        { /* GRID Component here??? props is */ }
+        <div className="posts-grid col-md-12 d-flex justify-content-start flex-wrap">
+          {
+            this.state.visiblePosts.map(function (post, key) {
+              return (
+                <div key={ key } className="col-md-4 col-sm-12">
+                  <Card
+                    title={ post.title.rendered }
+                    body={ Card.formatHtmlContent(post.excerpt.rendered || 'No Content Available') }
+                    heading={ BlogPostsList.formatHeading(post) }
+                    link={ `/posts/${post.id}` }
+                    imgSource={ post.media_details.medium_large ? post.media_details.medium_large.source_url : undefined }
+                    imgAlt={ post.title.rendered }
+                    facebookShareMessage={ `/posts/${post.id}` }
+                    twitterShareMessage={ `${ post.title.rendered } - /posts/${ post.id } ! @ProvidenceGeeks` }
+                  />
+                </div>
+              );
+            })
+          }
+        </div>
+
+        <LoadMoreButton loadMore={ () => this.loadMorePosts() }/>
+
+      </div>
+
+    );
+  }
+}
+
+BlogPostsList.propTypes = {
+  children: PropTypes.element
 };
-
-export default BlogPostsList;
