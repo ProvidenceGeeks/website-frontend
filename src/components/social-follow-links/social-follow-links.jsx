7e989825c1772302dbdbcf8aab777bf7d8efc994
emyarod <afw5059@gmail.com>
2018-01-21 15:13:35 -0500

    Convert social links component to stateless function

--- src/components/social-follow-links/social-follow-links.jsx
+++ src/components/social-follow-links/social-follow-links.jsx
@@ -4,28 +4,26 @@ import SlackIcon from '../slack-icon/slack-icon';
 import TwitterIcon from '../twitter-icon/twitter-icon';
 import './social-follow-links.scss';
 
-export default class SocialFollowLinks extends React.Component {
-  render() {
-    return (
-      <div className="social-container d-flex align-items-center">
-        <ul className="social list-inline d-flex">
-          <li>
-            <a className="facebook-link" href="https://www.facebook.com/providencegeeks/" title="Like Us" target="_blank" rel="noopener noreferrer">
-              <FacebookIcon />
-            </a>
-          </li>
-          <li>
-            <a className="slack-link" href="http://providencegeeks.slack.com/" title="Join Us" target="_blank" rel="noopener noreferrer">
-              <SlackIcon />
-            </a>
-          </li>
-          <li>
-            <a className="twitter-link" href="https://twitter.com/providencegeeks" title="Follow Us" target="_blank" rel="noopener noreferrer">
-              <TwitterIcon />
-            </a>
-          </li>
-        </ul>
-      </div>
-    );
-  }
+export default function SocialFollowLinks() {
+  return (
+    <div className="social-container d-flex align-items-center">
+      <ul className="social list-inline d-flex">
+        <li>
+          <a className="facebook-link" href="https://www.facebook.com/providencegeeks/" title="Like Us" target="_blank" rel="noopener noreferrer">
+            <FacebookIcon />
+          </a>
+        </li>
+        <li>
+          <a className="slack-link" href="http://providencegeeks.slack.com/" title="Join Us" target="_blank" rel="noopener noreferrer">
+            <SlackIcon />
+          </a>
+        </li>
+        <li>
+          <a className="twitter-link" href="https://twitter.com/providencegeeks" title="Follow Us" target="_blank" rel="noopener noreferrer">
+            <TwitterIcon />
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 }
