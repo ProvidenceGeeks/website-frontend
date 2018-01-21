7ccc9956ee5e0a42f22fd8261f447f1ad68ea224
emyarod <afw5059@gmail.com>
2018-01-21 15:08:31 -0500

    Remove Bootstrap grid classes Remove `!important` rules

--- src/components/social-follow-links/social-follow-links.jsx
+++ src/components/social-follow-links/social-follow-links.jsx
@@ -7,15 +7,25 @@ import './social-follow-links.scss';
 export default class SocialFollowLinks extends React.Component {
   render() {
     return (
-
-      <div className="social-container col-md-8 offset-md-2 d-flex justify-content-end align-items-center">
+      <div className="social-container d-flex align-items-center">
         <ul className="social list-inline d-flex">
-          <li><a className="facebook-link" href="https://www.facebook.com/providencegeeks/" title="Like Us" target="_blank" rel="noopener noreferrer"><FacebookIcon /></a></li>
-          <li><a className="slack-link" href="http://providencegeeks.slack.com/" title="Join Us" target="_blank" rel="noopener noreferrer"><SlackIcon /></a></li>
-          <li><a className="twitter-link" href="https://twitter.com/providencegeeks" title="Follow Us" target="_blank" rel="noopener noreferrer"><TwitterIcon /></a></li>
+          <li>
+            <a className="facebook-link" href="https://www.facebook.com/providencegeeks/" title="Like Us" target="_blank" rel="noopener noreferrer">
+              <FacebookIcon />
+            </a>
+          </li>
+          <li>
+            <a className="slack-link" href="http://providencegeeks.slack.com/" title="Join Us" target="_blank" rel="noopener noreferrer">
+              <SlackIcon />
+            </a>
+          </li>
+          <li>
+            <a className="twitter-link" href="https://twitter.com/providencegeeks" title="Follow Us" target="_blank" rel="noopener noreferrer">
+              <TwitterIcon />
+            </a>
+          </li>
         </ul>
       </div>
-
     );
   }
 }
