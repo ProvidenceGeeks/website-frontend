e9b92052e3e75a5adab450752bafc89b198a0ff2
jlegeros <jlegeros@gmail.com>
2019-10-24 20:45:16 -0400

    removed 'prettier' formating which looked like it was breaking stuff

--- src/components/social-follow-links/social-follow-links.jsx
+++ src/components/social-follow-links/social-follow-links.jsx
@@ -1,55 +1,33 @@
-import * as React from "react";
-import FacebookIcon from "../facebook-icon/facebook-icon";
-import SlackIcon from "../slack-icon/slack-icon";
-import TwitterIcon from "../twitter-icon/twitter-icon";
-import GitHubIcon from "../github-icon/github-icon";
-import "./social-follow-links.scss";
+import * as React from 'react';
+import FacebookIcon from '../facebook-icon/facebook-icon';
+import SlackIcon from '../slack-icon/slack-icon';
+import TwitterIcon from '../twitter-icon/twitter-icon';
+import GitHubIcon from '../github-icon/github-icon';
+import './social-follow-links.scss';
 
 export default function SocialFollowLinks() {
   return (
     <div className="social-container d-flex align-items-center">
       <ul className="social list-inline d-flex">
         <li>
-          <a
-            className="facebook-link"
-            href="https://www.facebook.com/providencegeeks/"
-            title="Like Us"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a className="facebook-link" href="https://www.facebook.com/providencegeeks/" title="Like Us" target="_blank" rel="noopener noreferrer">
             <FacebookIcon />
           </a>
         </li>
         <li>
-          <a
-            className="slack-link"
+          <a className="slack-link"
             href="https://join.slack.com/t/providencegeeks/shared_invite/enQtMzI4MjA2Mzk5MDk1LWMxNDEzZDVmOWFhZDcwMmJiYTdjOGM1NTZmMzJlMGFiMWI0ODY5NTVmMGFhODQzYmY1NjkyNTJiNDU5ZDNlZmM"
-            title="Join Us"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+            title="Join Us" target="_blank" rel="noopener noreferrer">
             <SlackIcon />
           </a>
         </li>
         <li>
-          <a
-            className="twitter-link"
-            href="https://twitter.com/providencegeeks"
-            title="Follow Us"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a className="twitter-link" href="https://twitter.com/providencegeeks" title="Follow Us" target="_blank" rel="noopener noreferrer">
             <TwitterIcon />
           </a>
         </li>
         <li>
-          <a
-            className="github-link"
-            href="https://github.com/ProvidenceGeeks/"
-            title="Collaborate With Us"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a className="github-link" href="https://github.com/ProvidenceGeeks/" title="Collaborate With Us" target="_blank" rel="noopener noreferrer">
             <GitHubIcon />
           </a>
         </li>
