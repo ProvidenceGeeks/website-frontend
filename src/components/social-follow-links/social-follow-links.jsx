461f4ed40ef806befee1d2a185a8cd89cf1ec39f
Paul Barry <devlab2425@gmail.com>
2019-10-28 21:04:20 -0400

    Add aria-label to social buttons

--- src/components/social-follow-links/social-follow-links.jsx
+++ src/components/social-follow-links/social-follow-links.jsx
@@ -10,24 +10,50 @@ export default function SocialFollowLinks() {
     <div className="social-container d-flex align-items-center">
       <ul className="social list-inline d-flex">
         <li>
-          <a className="facebook-link" href="https://www.facebook.com/providencegeeks/" title="Like Us" target="_blank" rel="noopener noreferrer">
+          <a 
+            className="facebook-link" 
+            href="https://www.facebook.com/providencegeeks/" 
+            title="Like PVDGeeks on Facebook" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            aria-label="Like PVDGeeks on Facebook"
+          >
             <FacebookIcon />
           </a>
         </li>
         <li>
-          <a className="slack-link"
+          <a 
+            className="slack-link"
             href="https://join.slack.com/t/providencegeeks/shared_invite/enQtMzI4MjA2Mzk5MDk1LWMxNDEzZDVmOWFhZDcwMmJiYTdjOGM1NTZmMzJlMGFiMWI0ODY5NTVmMGFhODQzYmY1NjkyNTJiNDU5ZDNlZmM"
-            title="Join Us" target="_blank" rel="noopener noreferrer">
+            title="Join the PVDGeeks Slack channel" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            aria-label="Join the PVDGeeks Slack channel"
+          >
             <SlackIcon />
           </a>
         </li>
         <li>
-          <a className="twitter-link" href="https://twitter.com/providencegeeks" title="Follow Us" target="_blank" rel="noopener noreferrer">
+          <a 
+            className="twitter-link" 
+            href="https://twitter.com/providencegeeks" 
+            title="Follow Us on Twitter" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            aria-label="Follow Us on Twitter" 
+          >
             <TwitterIcon />
           </a>
         </li>
         <li>
-          <a className="github-link" href="https://github.com/ProvidenceGeeks/" title="Collaborate With Us" target="_blank" rel="noopener noreferrer">
+          <a 
+            className="github-link" 
+            href="https://github.com/ProvidenceGeeks/" 
+            title="Collaborate With Us" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            aria-label="Contribute on GitHub"
+          >
             <GitHubIcon />
           </a>
         </li>
